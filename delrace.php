0527d41bd0ff2fa8faefc02629aac5b8f90fc7dd
GhaidaAlkindi <163113770+GhaidaAlkindi@users.noreply.github.com>
2024-05-06 10:41:14 +0400

    Update delrace.php

--- delrace.php
+++ delrace.php
@@ -1,16 +1,20 @@
 <?php
+// Include the connection file
 require_once "connection.php";
 
+// Get the round value from the POST request
 $round = $_POST['round1'];
+
+// Construct the SQL query to delete a race with the specified round
 $sql = "DELETE FROM races WHERE round = '$round'";
 
+// Execute the SQL query and check if it was successful
 if ($conn->query($sql) === TRUE) {
-    echo "Race deleted successfully!";
-} else {
-    echo  $conn->error;
+    echo "Race deleted successfully!";  // If successful, display a success message
+} else {  
+    echo  $conn->error; // If an error occurred, display the error message
 }
 
-
+// Close the database connection
 $conn->close();
 ?>
- 
