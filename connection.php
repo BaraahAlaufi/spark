fb1e02d750e2ab56586d70351156de30bb76b208
GhaidaAlkindi <163113770+GhaidaAlkindi@users.noreply.github.com>
2024-05-07 19:06:16 +0400

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,4 +1,5 @@
 <?php
+//connected to file races.sql
 $conn = new mysqli("localhost", "root", "", "mysql"); //connecting to database
 if ($conn->connect_error) { //check connection
     die("connection failed: " . $conn->connect_error); //close the PHP tag
